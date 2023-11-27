d919312eb6e40bed5862b5e024ade675b0bddb1f
coitox <152063968+coitox@users.noreply.github.com>
2023-11-27 18:39:06 +0530

    Create insta.py

---
+++ AnonXMusic/plugins/tools/insta.py
@@ -0,0 +1,35 @@
+import os
+import asyncio
+from pyrogram import filters
+from pyrogram.types import Message
+from AnonXMusic import bot 
+
+
+# reels download 
+
+
+@bot.on_message(filters.command(["ig"], ["/", "!", "."]))
+async def download_instareels(c: bot, m: Message):
+    try:
+        reel_ = m.command[1]
+    except IndexError:
+        await m.reply_text("Give me an link to download it...")
+        return
+    if not reel_.startswith("https://www.instagram.com/reel/"):
+        await m.reply_text("In order to obtain the requested reel, a valid link is necessary. Kindly provide me with the required link.")
+        return
+    OwO = reel_.split(".",1)
+    Reel_ = ".dd".join(OwO)
+    try:
+        await m.reply_video(Reel_)
+        return
+    except Exception:
+        try:
+            await m.reply_photo(Reel_)
+            return
+        except Exception:
+            try:
+                await m.reply_document(Reel_)
+                return
+            except Exception:
+                await m.reply_text("I am unable to reach to this reel.")
