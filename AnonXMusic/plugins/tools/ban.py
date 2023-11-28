7629c4049bddcf1acd32f9ef804948cead3c8d7f
coitox <152063968+coitox@users.noreply.github.com>
2023-11-28 12:36:21 +0530

    Update ban.py

--- AnonXMusic/plugins/tools/ban.py
+++ AnonXMusic/plugins/tools/ban.py
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["ban"], prefixes=["/"]) & admin_filter)
+@app.on_message(filters.command(["umiko", "ban"], prefixes=["y", "Y"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
